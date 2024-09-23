9616cf89bba06928d1b407d16a67558b3ad22ad6
IVAN-SMI <Van96.Smirnov@gmail.com>
2024-09-23 23:10:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите свое имя");
+string? per1 = Console.ReadLine();
+
+Console.WriteLine("Введите свою фамилию");
+string? per2 = Console.ReadLine();
+
+Console.WriteLine("Введите свое отчество");
+string? per3 = Console.ReadLine();
+
+string? text = $"Здравствуйте {per1} {per2} {per3}!";
+Console.WriteLine(text);
+
+Console.ReadKey();
